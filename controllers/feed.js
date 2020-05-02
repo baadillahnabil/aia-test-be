c3e91146b3e15c14d54976feb67577150ed0e498
Nabil Baadillah <nabil.baadillah@transdigital.co.id>
2020-05-02 22:42:38 +0700

    generate readable json and send the json

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,13 +1,32 @@
 /* eslint-disable import/prefer-default-export */
 import axios from 'axios'
 import xml2js from 'xml2js'
+import { map, find } from 'lodash'
 
 export const getFeeds = (req, res) => {
   axios
     .get('https://api.flickr.com/services/feeds/photos_public.gne')
     .then((response) =>
       xml2js.parseString(response.data, (error, result) => {
-        res.status(200).send(result.feed.entry[0].content[0]._)
+        res.status(200).json({
+          success: true,
+          data: map(result.feed.entry, (data) => ({
+            author: {
+              name: data.author[0].name,
+              avatar: data.author[0]['flickr:buddyicon'],
+              url: data.author[0].uri
+            },
+            content: {
+              image: find(data.link, (link) => link.$.rel === 'enclosure').$
+                .href,
+              source: find(data.link, (link) => link.$.rel === 'alternate').$
+                .href
+            },
+            title: data.title[0],
+            published_date: data.published[0],
+            updated_date: data.updated[0]
+          }))
+        })
       })
     )
     .catch(() => {
