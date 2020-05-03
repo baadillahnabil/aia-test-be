a3314a2d26ec166d0434a2fe637b0689329c00f5
Nabil Baadillah <nabil.baadillah@transdigital.co.id>
2020-05-03 21:33:19 +0700

    add cors handling

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import { config } from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 
 import IndexRouter from './routes'
 import NotFound from './routes/404'
 
 const app = express()
+
+// Handle CORS
+app.use(cors())
 
 app.use('/', IndexRouter)
 app.use(NotFound)
